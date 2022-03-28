7a16e8be79626e2501fe7129e5c9252671dc57a1
Mr. Joke <101518570+Temlure@users.noreply.github.com>
2022-03-28 08:37:58 +0800

    Trade history bitstamp fastcoin

--- src/pair.rs
+++ src/pair.rs
@@ -1,17 +1,5 @@
 #[derive(Debug)]
 #[derive(PartialEq)]
-#[allow(non_camel_case_types)]
 pub enum Pair {
-    BTC_EUR,
-    BTC_USD,
-    ETC_BTC,
-    ETC_EUR,
-    ETC_USD,
-    ETH_BTC,
-    ETH_EUR,
-    ETH_USD,
-    EUR_USD,
-    XRP_BTC,
-    XRP_EUR,
-    XRP_USD,
+    BtcUsd,
 }
