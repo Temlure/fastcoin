e48bf9be5cb45d40356efd03723d816bea5d0552
Mr. Joke <101518570+Temlure@users.noreply.github.com>
2022-04-08 00:59:12 +0800

    Update the TODO list and add missing doc for modules

--- examples/generic_api.rs
+++ examples/generic_api.rs
@@ -6,10 +6,11 @@ extern crate fastcoin;
 
 use fastcoin::fastcoin::Fastcoin;
 use fastcoin::exchange::Exchange::*;
+use fastcoin::pair::Pair;
 
 fn main() {
     // We create a Fastcoin Generic API
-    // as Poloniex does not need customer_id field, we keep it empty
-    let mut _my_api = Fastcoin::new(Poloniex, "", "api_key", "api_secret");
-
+    // Since Poloniex does not need customer_id field, we keep it empty
+    let mut my_api = Fastcoin::new(Poloniex, "", "api_key", "api_secret");
+    my_api.return_balances(Pair::ETC_BTC);
 }
