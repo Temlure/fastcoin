7f86a5547d5629c42634565e0df9c012c70369f1
Mr. Joke <101518570+Temlure@users.noreply.github.com>
2022-04-23 07:52:19 +0800

    Prepare version 0.2 (2)

--- examples/generic_api.rs
+++ examples/generic_api.rs
@@ -1,6 +1,8 @@
-// This example shows how to use the generic API provided by fastcoin
+// This example shows how to use the generic API provided by Fastcoin.
 // This method is useful if you have to iterate throught multiple accounts of
 // different exchanges and perform the same operation (such as get the current account's balance)
+// You can also use the Fastcoin generic API if you want a better error handling since all methods
+// return Result<_, Error>.
 
 extern crate fastcoin;
 
@@ -10,7 +12,9 @@ use fastcoin::pair::Pair::*;
 
 fn main() {
     // We create a Fastcoin Generic API
-    // Since Poloniex does not need customer_id field, we keep it empty
-    let mut my_api = Fastcoin::new(Poloniex, "", "api_key", "api_secret");
-    my_api.ticker(ETC_BTC);
+    // Since Kraken does not need customer_id field, we set it to None
+    let mut my_api = Fastcoin::new(Kraken, "api_key", "api_secret", None);
+    let ticker = my_api.ticker(ETC_BTC);
+
+    println!("ETC_BTC last trade price is {}.", ticker.unwrap().last_trade_price);
 }
